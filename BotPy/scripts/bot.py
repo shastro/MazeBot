fd0726879de0d9063bba23a8718cb9f8b321c23b
shastro <guitarshughes@hotmail.com>
2018-12-20 21:24:14 -0700

    Secrets processing

--- BotPy/scripts/bot.py
+++ BotPy/scripts/bot.py
@@ -1,4 +1,15 @@
+import discord
 
+# https://discordapp.com/api/oauth2/authorize?client_id=525510742770843678&permissions=100416&scope=bot
+# BOT URL, Replace client_id with your own
 
+# Import "Secret" data
+# --NOTE--
+# secrets.txt should contain only two lines, the first being the bot token, and the second being the client secret
 secrets = open('secrets.txt', 'r')
-print(secrets)
+BOT_TOKEN = secrets.readline()
+CLIENT_SECRET = secrets.readline()
+print(BOT_TOKEN)
+print(CLIENT_SECRET)
+
+secrets.close()
