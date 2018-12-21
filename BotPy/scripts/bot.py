6746c15915d2528b24d023ef0b08455e5cb67a2b
shastro <guitarshughes@hotmail.com>
2018-12-20 22:25:18 -0700

    Working Discord Bot, Added Event Handling, and basic commands

--- BotPy/scripts/bot.py
+++ BotPy/scripts/bot.py
@@ -8,8 +8,32 @@ import discord
 # secrets.txt should contain only two lines, the first being the bot token, and the second being the client secret
 secrets = open('secrets.txt', 'r')
 BOT_TOKEN = secrets.readline()
-CLIENT_SECRET = secrets.readline()
-print(BOT_TOKEN)
-print(CLIENT_SECRET)
-
+CLIENT_SECRET = str(secrets.readline())
 secrets.close()
+
+# Stripping Trailing Newline char from BOT_TOKEN
+BOT_TOKEN = BOT_TOKEN.rstrip()
+
+# Create Client
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+    print(f"We have logged in as {client.user}")
+
+
+@client.event
+async def on_message(message):
+    print(f"{message.channel}: {message.author.name}: {message.content}")
+
+    if "!maze" in message.content.lower():
+        await message.channel.send("Hi")
+
+
+
+    if "!end" == message.content.lower():
+        await client.close()
+
+
+client.run(BOT_TOKEN)
